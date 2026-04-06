4d102f6677ab5924eb4033c4020f60b9c3beaecb
George Lupu <117625145+Puk3p@users.noreply.github.com>
2026-04-07 01:25:56 +0300

    feat: initiate repo

---
+++ src/main/kotlin/ro/puk3p/polaraddon/PolarAddonPlugin.kt
@@ -0,0 +1,24 @@
+package ro.puk3p.polaraddon
+
+import org.bukkit.plugin.java.JavaPlugin
+import ro.puk3p.polaraddon.polar.PolarApiHook
+
+class PolarAddonPlugin : JavaPlugin() {
+    companion object {
+        lateinit var instance: PolarAddonPlugin
+            private set
+    }
+
+    override fun onLoad() {
+        instance = this
+        top.polar.api.loader.LoaderApi.registerEnableCallback(PolarApiHook::init)
+    }
+
+    override fun onEnable() {
+        logger.info("PolarAddon enabled.")
+    }
+
+    override fun onDisable() {
+        logger.info("PolarAddon disabled.")
+    }
+}
---
+++ src/main/kotlin/ro/puk3p/polaraddon/polar/PolarApiHook.kt
@@ -0,0 +1,52 @@
+package ro.puk3p.polaraddon.polar
+
+import ro.puk3p.polaraddon.PolarAddonPlugin
+import top.polar.api.PolarApiAccessor
+import top.polar.api.event.listener.repository.EventListenerRepository
+import top.polar.api.user.event.DetectionAlertEvent
+import top.polar.api.user.event.MitigationEvent
+import top.polar.api.user.event.PunishmentEvent
+
+
+object PolarApiHook {
+    fun init() {
+        val plugin = PolarAddonPlugin.instance
+        plugin.logger.info("Polar API initialised — registering event listeners.")
+
+        val api =
+            PolarApiAccessor.access().get()
+                ?: run {
+                    plugin.logger.severe("Polar API is unavailable after init callback — aborting.")
+                    return
+                }
+
+        registerListeners(api.events().repository())
+    }
+
+    private fun registerListeners(repo: EventListenerRepository) {
+        val plugin = PolarAddonPlugin.instance
+
+        repo.registerListener(DetectionAlertEvent::class.java) { event ->
+            // TODO: handle detection alerts
+            plugin.logger.info(
+                "[Polar] Detection alert: player=${event.user().username()}, " +
+                    "check=${event.check().name()}, vl=${event.check().violationLevel()}",
+            )
+        }
+
+        repo.registerListener(MitigationEvent::class.java) { event ->
+            // TODO: handle mitigations (cancelled packets / rubber-banding)
+            plugin.logger.info(
+                "[Polar] Mitigation: player=${event.user().username()}, " +
+                    "check=${event.check().name()}",
+            )
+        }
+
+        repo.registerListener(PunishmentEvent::class.java) { event ->
+            // TODO: handle punishments
+            plugin.logger.info(
+                "[Polar] Punishment: player=${event.user().username()}",
+            )
+        }
+    }
+}
